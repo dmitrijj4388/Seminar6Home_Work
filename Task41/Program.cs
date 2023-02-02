8542c3cad5501772458e5666e6465f78aae32d1e
dmitrijj4388 <dmitrijj4388@mail.ru>
2023-02-03 00:08:48 +0300

    Home work add

---
+++ Task41/Program.cs
@@ -0,0 +1,63 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+// 0, 7, 8, -2, -2 -> 2
+
+// 1, -7, 567, 89, 223-> 3
+
+
+ 
+
+int[] ConvToIntArr(string[] num1)
+{
+   int a = num1.Length;
+   int[] newArray = new int[a];
+   for (int i = 0;i < a; i++)
+   {
+      bool result = int.TryParse(num1[i], out int b);
+      if (result)
+      {
+        newArray[i] = int.Parse(num1[i]);
+
+      }
+      else
+      {
+       newArray[i] = 0;
+      }
+     
+   }
+
+
+   return newArray;
+}
+
+int CountPositive(int[] arr)
+{
+   int count = 0;
+   for (int i = 0; i < arr.Length; i++)
+   {
+      if (arr[i] > 0) count++;
+   }
+   return count;
+}
+
+
+Console.Clear();
+
+Console.WriteLine("Введите числа через запятую: ");
+string numbers = Console.ReadLine()!;
+string[] num1 = numbers.Split(',', ' ');
+int[] newNum = ConvToIntArr(num1);
+int count = CountPositive(newNum);
+
+System.Console.WriteLine($"Положительных чисел {count}.");
+
+
+
+
+
+
+
+
+
+
+
---
+++ Task43/Program.cs
@@ -0,0 +1,48 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+double CoordinatesX(double k1,double b1,double k2,double b2)
+{
+    double x = (b2 - b1)/(k1 - k2);
+    return x;
+}
+
+double CoordinatesY(double k1,double b1,double x)
+{
+    double y = k1*x + b1;
+    return y;
+}
+
+
+Console.Clear();
+
+Console.WriteLine("Введите значение k1");
+double k1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите значение b1");
+double b1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите значение k2");
+double k2 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите значение b2");
+double b2 = Convert.ToDouble(Console.ReadLine());
+
+double x = CoordinatesX(k1, b1, k2, b2);
+double y = CoordinatesY(k1, b1, x);
+
+if (k1 - k2 == 0 & b1 - b2 != 0) Console.WriteLine("Прямые не пересекаються.");
+else
+{
+  if (k1 - k2 == 0 & b1 - b2 == 0 ) Console.WriteLine("Прямые совпадают.");
+  else Console.WriteLine($"Прямые пересекаються в точке ({x}, {y})");
+}
+   
+
+
+
+
+
+
+
+
+
+
